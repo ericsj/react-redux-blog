bc42931ec0a3a5f38631ce2cdb38e386792ff6b3
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2023-08-22 12:51:15 -0300

    add new actions

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
 
 function Counter() {
   const count = useSelector((state) => state.counter.count);
+  const [incrementAmount, setIncrementAmount] = useState();
   const dispatch = useDispatch();
-
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
   return (
     <section>
       <p>{count}</p>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={resetAll}>Reset</button>
+        <input
+          type="number"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+        >
+          Increment
+        </button>
       </div>
     </section>
   );
