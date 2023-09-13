701c95c8492200be879817d0a99aa6d475cdee20
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2023-09-13 11:25:18 -0300

    add timeAgo

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from '../posts/PostAuthor'
+import { sub } from 'date-fns'
+import TimeAgo from './TimeAgo'
 
 function PostsList() {
   const posts = useSelector(selectAllPosts);
@@ -12,6 +14,7 @@ function PostsList() {
       <p>{post.content}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,14 +1,17 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: "1",
     title: "Learning Redux",
+    date: sub(new Date(), {minutes: 10}).toISOString(),
     content: "I' m watch a YT tutorial about Redux with React",
   },
   {
     id: "2",
     title: "Learning React",
+    date: sub(new Date(), {minutes: 10}).toISOString(),
     content: "I' m watch a YT tutorial about React",
   },
 ];
@@ -27,6 +30,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
           },
         };
