86ed655a8f36786f1a76a0dc7764c9382e0dcddc
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2023-09-21 12:16:53 -0300

    create PostExcerpt

---
+++ src/features/posts/PostsExcerpt.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
+
+function PostsExcerpt({ post }) {
+    return (
+        <article >
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+                <ReactionButtons post={post} />
+            </p>
+        </article>)
+}
+
+export default PostsExcerpt
--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -6,23 +6,22 @@ import { sub } from 'date-fns'
 import TimeAgo from './TimeAgo'
 import ReactionButtons from './ReactionButtons'
 import { useDispatch } from "react-redux";
+import PostExcerpt from './PostsExcerpt'
 
 function PostsList() {
   const dispatch = useDispatch()
   const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-        <ReactionButtons post={post} />
-      </p>
-    </article>
-  ));
+  useEffect(() => {
+    if (postsStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postsStatus, dispatch])
+
+  const renderedPosts = orderedPosts.map((post) => <PostExcerpt post={post} />);
   return (
     <section>
       <h2>Posts</h2>
